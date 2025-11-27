34d79559dfe77ad40bb835075e9d3409ba5d7f3a
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2025-11-26 23:16:25 -0800

    chore: use more moder GivenWhenThen tests
    this is what I use in larger projects to make the tests more flexible
    to adding additional colaborators

--- internal/hypertext/template_routes_test.go
+++ internal/hypertext/template_routes_test.go
@@ -15,26 +15,60 @@ import (
 )
 
 func TestTemplates(t *testing.T) {
-	for _, tt := range []struct {
-		Name     string
-		Request  func(rsv *fake.Server) *http.Request
-		Response func(rsv *fake.Server, res *http.Response)
-	}{
+	type (
+		Given struct {
+			srv *fake.Server
+		}
+		When struct{}
+		Then struct {
+			res *http.Response
+			srv *fake.Server
+		}
+		Case struct {
+			Name  string
+			Given func(*testing.T, *Given)
+			When  func(*testing.T, *When) *http.Request
+			Then  func(*testing.T, *Then)
+		}
+	)
+
+	run := func(tc Case) func(t *testing.T) {
+		return func(t *testing.T) {
+			srv := new(fake.Server)
+			mux := http.NewServeMux()
+			hypertext.TemplateRoutes(mux, srv)
+			rec := httptest.NewRecorder()
+
+			if tc.Given != nil {
+				tc.Given(t, &Given{srv})
+			}
+			req := tc.When(t, &When{})
+			mux.ServeHTTP(rec, req)
+			tc.Then(t, &Then{
+				res: rec.Result(),
+				srv: srv,
+			})
+		}
+	}
+
+	for _, tc := range []Case{
 		{
 			Name: "the header has the name",
-			Request: func(srv *fake.Server) *http.Request {
-				srv.IndexReturns(hypertext.IndexData{
+			Given: func(t *testing.T, g *Given) {
+				g.srv.IndexReturns(hypertext.IndexData{
 					Name: "somebody",
 				})
+			},
+			When: func(t *testing.T, _ *When) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/", nil)
 			},
-			Response: func(rsv *fake.Server, res *http.Response) {
-				if assert.Equal(t, 1, rsv.IndexCallCount()) {
-					ctx := rsv.IndexArgsForCall(0)
+			Then: func(t *testing.T, then *Then) {
+				if assert.Equal(t, 1, then.srv.IndexCallCount()) {
+					ctx := then.srv.IndexArgsForCall(0)
 					require.NotNil(t, ctx)
 				}
-				assert.Equal(t, http.StatusOK, res.StatusCode)
-				doc := domtest.ParseResponseDocument(t, res)
+				assert.Equal(t, http.StatusOK, then.res.StatusCode)
+				doc := domtest.ParseResponseDocument(t, then.res)
 				if el := doc.QuerySelector(`h1`); assert.NotNil(t, el) {
 					assert.Equal(t, "Hello, somebody!", strings.TrimSpace(el.TextContent()))
 				}
@@ -42,26 +76,18 @@ func TestTemplates(t *testing.T) {
 		},
 		{
 			Name: "the about page is routable",
-			Request: func(srv *fake.Server) *http.Request {
+			When: func(t *testing.T, _ *When) *http.Request {
 				return httptest.NewRequest(http.MethodGet, "/about", nil)
 			},
-			Response: func(rsv *fake.Server, res *http.Response) {
-				assert.Equal(t, http.StatusOK, res.StatusCode)
-				doc := domtest.ParseResponseDocument(t, res)
+			Then: func(t *testing.T, then *Then) {
+				assert.Equal(t, http.StatusOK, then.res.StatusCode)
+				doc := domtest.ParseResponseDocument(t, then.res)
 				if el := doc.QuerySelector(`h1`); assert.NotNil(t, el) {
 					assert.Equal(t, "About", strings.TrimSpace(el.TextContent()))
 				}
 			},
 		},
 	} {
-		t.Run(tt.Name, func(t *testing.T) {
-			srv := new(fake.Server)
-			mux := http.NewServeMux()
-			hypertext.TemplateRoutes(mux, srv)
-			rec := httptest.NewRecorder()
-			req := tt.Request(srv)
-			mux.ServeHTTP(rec, req)
-			tt.Response(srv, rec.Result())
-		})
+		t.Run(tc.Name, run(tc))
 	}
 }
