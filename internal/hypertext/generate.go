c821b484dd9184209752ef1dd36487ae7376cddb
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2025-12-21 00:34:20 -0800

    chore: bump muxt to v0.18.1
    notice how the flags still work.

--- internal/hypertext/generate.go
+++ internal/hypertext/generate.go
@@ -1,6 +1,6 @@
 package hypertext
 
-//go:generate go run github.com/typelate/muxt/cmd/muxt generate --receiver-type=Server --routes-func=TemplateRoutes
+//go:generate go run github.com/typelate/muxt/cmd/muxt generate --use-receiver-type=Server
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 
 //counterfeiter:generate -o=../fake/server.go --fake-name=Server . RoutesReceiver
